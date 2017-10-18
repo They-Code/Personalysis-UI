0822ef09e1226453501f3f6325f1291fe81d38ae
Steven Jing <steven.jing@gmail.com>
2017-10-18 16:30:25 -0700

    Add signup page files

---
+++ src/Signup.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Signup.css';
+
+class Signup extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+    
+    render() {
+        return (
+            <h1>Signup</h1>
+        );
+    }
+}
+
+export default Signup;
