0bd587e4b0f6149ed702117491f0b210af1e5d4a
maxbelm <belman.maxim@gmail.com>
2017-10-21 14:41:51 -0700

    built landing page gradient and correct dimensions

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import '../styles/common.css';
+import Container from 'muicss/lib/react/container';
+import Row from 'muicss/lib/react/row';
+import Col from 'muicss/lib/react/col';
 
 class Home extends Component {
     constructor() {
@@ -9,7 +12,34 @@ class Home extends Component {
     
     render() {
         return (
-            <h1>Home</h1>
+            <Container className="landing-main-container">
+                <Row className="landing-main">
+
+                    <Col md="12">md-12
+                        <br/>
+                        <br/>
+                        <h1 className="landing-main-tagline">
+                            Who You Are
+                            <br/>
+                            Is In Your
+                            <br/>
+                            DNA
+                        </h1>
+                    </Col>
+                </Row>
+                 <Row>
+                    <Col md="12">
+                        Row2-md-12
+                        <br/>
+                    </Col>
+                 </Row>
+                 <Row>
+                    <Col md="12">Row3-md-12</Col>
+                </Row>
+                <Row>
+                    <Col md="12">Row4-md-12</Col>
+                </Row>
+            </Container>
         );
     }
 }
