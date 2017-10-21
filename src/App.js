e76eb134b665e4a2ce0b2ac47e0b61112628379c
Steven Jing <steven.jing@gmail.com>
2017-10-21 12:09:04 -0700

    Create Home component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react';
 import './styles/common.css';
+import Home from './components/Home.js';
 import Login from './components/Login.js';
 import { 
   BrowserRouter as Router,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
-
-const Nav = () => (
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-    <Link to="/login">Login</Link>
-  </nav>
-);
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Nav />
-          <Route exact path='/' render={() => <h1>Home</h1>} />
+          <Route exact path='/' component={Home} />
           <Route path='/about' render={() => <h1>About</h1>} />
           <Route path='/login' component={Login} />
         </div>
