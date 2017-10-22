f5331896a09c57810b90b28dba9d2f1e97ed55fe
Steven Jing <steven.jing@gmail.com>
2017-10-22 16:56:29 -0700

    Style navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import '../styles/common.css';
+
 import {
   Container,
   Row,
@@ -13,38 +15,40 @@ import {
 class Nav extends Component {
   render() {
     return (
-      <Container className="nav" fluid={true}>
+      <Container className="navbar" fluid={true}>
         <Row>
           <Col md="12">
 
           <AppBar
             title="Personalystic"
             titleStyle={{
-              'maxWidth' : '17%'
-              }}
+              flex: 'none',
+              marginRight: '0.5em',
+            }}
             iconStyleRight={{
-              'marginLeft': 'auto',
-              'marginRight': 'auto',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              width: '100%',
             }}
             iconElementRight={
               <Container fluid={true}>
                 <Row>
-                  <Col md="2">
+                  <Col md="1">
                     <FlatButton>FEATURES</FlatButton>
                   </Col>
-                  <Col md="2">
+                  <Col md="1">
                     <FlatButton>CONTACT</FlatButton>
                   </Col>
-                  <Col md="2">
+                  <Col md="1">
                     <FlatButton>ABOUT US</FlatButton>
                   </Col>
-                  <Col md="2">
+                  <Col md="4">
                     <FlatButton>BLOG</FlatButton>
                   </Col>
-                  <Col md="2">
+                  <Col md="4" className="login-button">
                     <FlatButton>LOGIN</FlatButton>
                   </Col>
-                  <Col md="2">
+                  <Col md="1">
                     <FlatButton>SIGN UP</FlatButton>
                   </Col>
                 </Row>
