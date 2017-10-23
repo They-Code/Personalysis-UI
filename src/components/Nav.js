f01f7a00743b09c64293355e7bbe6e3411e3cb11
Steven Jing <steven.jing@gmail.com>
2017-10-23 15:08:45 -0700

    Add click handlers to navbar items

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,22 +13,63 @@ import {
 } from 'material-ui';
 
 class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.onTitleTouchTapHandler = this.onTitleTouchTapHandler.bind(this);
+    this.onFeaturesButtonClick = this.onFeaturesButtonClick.bind(this);
+    this.onContactButtonClick = this.onContactButtonClick.bind(this);
+    this.onAboutUsButtonClick = this.onAboutUsButtonClick.bind(this);
+    this.onBlogButtonClick = this.onBlogButtonClick.bind(this);
+    this.onLoginButtonClick = this.onLoginButtonClick.bind(this);
+    this.onSignUpButtonClick = this.onSignUpButtonClick.bind(this);
+  }
+
+  onTitleTouchTapHandler() {
+    console.log('title clicked');
+  }
+
+  onFeaturesButtonClick() {
+    console.log('features clicked');
+  }
+  onContactButtonClick() {
+    console.log('contact clicked');
+  }
+
+  onAboutUsButtonClick() {
+    console.log('about us clicked');
+  }
+
+  onBlogButtonClick() {
+    console.log('blog clicked');
+  }
+
+  onLoginButtonClick() {
+    console.log('login clicked');
+  }
+
+  onSignUpButtonClick() {
+    console.log('signup clicked');
+  }
+  
   render() {
     return (
       <Container className="navbar" fluid={true}>
         <Row>
           <Col md="12">
-
           <AppBar
             style={{
               backgroundColor: 'white',
+              position: 'fixed',
             }}
-            title="Personalystic"
             titleStyle={{
               flex: 'none',
               marginRight: '0.5em',
               color: '#67B1C3'
             }}
+            onTitleTouchTap={this.onTitleTouchTapHandler}
+            title="Personalystic"
             iconStyleRight={{
               marginLeft: 'auto',
               marginRight: 'auto',
@@ -39,30 +80,28 @@ class Nav extends Component {
               <Container fluid={true}>
                 <Row>
                   <Col md="1">
-                    <FlatButton>FEATURES</FlatButton>
+                    <FlatButton onClick={this.onFeaturesButtonClick}>FEATURES</FlatButton>
                   </Col>
                   <Col md="1">
-                    <FlatButton>CONTACT</FlatButton>
+                    <FlatButton onClick={this.onContactButtonClick}>CONTACT</FlatButton>
                   </Col>
                   <Col md="1">
-                    <FlatButton>ABOUT US</FlatButton>
+                    <FlatButton onClick={this.onAboutUsButtonClick}>ABOUT US</FlatButton>
                   </Col>
                   <Col md="4">
-                    <FlatButton>BLOG</FlatButton>
+                    <FlatButton onClick={this.onBlogButtonClick}>BLOG</FlatButton>
                   </Col>
                   <Col md="4" className="login-button">
-                    <FlatButton>LOGIN</FlatButton>
+                    <FlatButton onClick={this.onLoginButtonClick}>LOGIN</FlatButton>
                   </Col>
                   <Col md="1" className="signup-button">
-                    <FlatButton>SIGN UP</FlatButton>
+                    <FlatButton onClick={this.onSignUpButtonClick}>SIGN UP</FlatButton>
                   </Col>
                 </Row>
               </Container>
             }
             showMenuIconButton={false}
           />
-
-
           </Col>
         </Row>
       </Container>
