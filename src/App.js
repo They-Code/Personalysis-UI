4fe56f767f1d5236e6d8d9c7b70d89a7f8f5f11f
Steven Jing <steven.jing@gmail.com>
2017-10-25 12:35:11 -0700

    Add login logic from Login to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import './styles/common.css';
-
-// material-ui
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-// react-router
-import { 
+import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
@@ -15,7 +11,41 @@ import Home from './components/Home.js';
 import Login from './components/Login.js';
 import ProfilePage from './components/ProfilePage.js';
 
+/**
+ * Binds props to a Component
+ * Use to bind props for Route Components
+ * @param {React.Component} Component - component to bind props to
+ * @param {Object} props - props to bind to Component
+ * @returns {Function} - function to be passed to the Route component
+ */
+const withProps = (Component, props) => {
+  return (matchProps) => <Component {...props} {...matchProps} />;
+};
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false,
+    };
+
+    this.updateLogin = this.updateLogin.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.isLoggedIn === true) {
+      return true;
+    }
+    return false;
+  }
+
+  updateLogin(isLoggedIn) {
+    this.setState({
+      isLoggedIn,
+    });
+  }
+
   render() {
     return (
       <MuiThemeProvider>
@@ -23,7 +53,7 @@ class App extends Component {
           <div>
             <Route exact path='/' component={Home} />
             <Route path='/about' render={() => <h1>About</h1>} />
-            <Route path='/login' component={Login} />
+            <Route path='/login' component={withProps(Login, {updateLogin: this.updateLogin})} />
             <Route path='/profile' component={ProfilePage} />
           </div>
         </Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import '../styles/common.css';
 
 class Login extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {};
+
+        this.login = this.login.bind(this);
     }
-    
+
+    login() {
+        this.props.updateLogin(true);
+    }
+
     render() {
         return (
-            <h1>Login</h1>
-
+            <div>
+                <h1>Login</h1>
+                <button type="button" onClick={this.login}>Login</button>
+            </div>
         );
     }
 }
