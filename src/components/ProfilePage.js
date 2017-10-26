bb608c00a64f331fe071d93de996077822d5971a
Steven Jing <steven.jing@gmail.com>
2017-10-25 20:27:42 -0700

    Add rough login logic to profile page

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import '../styles/profilePage.css';
 
+import Nav from './Nav.js';
 import ProfileInfo from './ProfileInfo.js';
 import StatisticalOverview from './StatisticalOverview.js';
-
-import Nav from './Nav.js';
 
 import {
     Container,
@@ -15,8 +14,28 @@ import {
 class ProfilePage extends Component {
     constructor(props) {
         super(props);
+
+        const isLoggedIn = this.props.location.state ?
+            Boolean(this.props.location.state.isLoggedIn) :
+            false;
+
+        this.state = {
+            isLoggedIn,
+        };
     }
-    
+
+    componentWillMount() {
+        if (!this.state.isLoggedIn) {
+            console.log('User is not logged in. Redirecting to login page');
+        }
+    }
+
+    componentDidMount() {
+        if (this.state.isLoggedIn) {
+            console.log('User is logged in. Fetching user Awakens data');
+        }
+    }
+
     render() {
         return (
             <div>
